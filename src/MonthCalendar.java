3f2cdce558626ad26a9627813eec2616a5882487
Andrew Heiko <geiko2015@gmail.com>
2017-04-06 15:51:32 +0300

    Formatted code. Removed int value day of week.

--- src/MonthCalendar.java
+++ src/MonthCalendar.java
@@ -13,7 +13,7 @@ public class MonthCalendar {
     public MonthCalendar(int monthNumber) {
         try {
             this.month = YearMonth.of(year, monthNumber);
-        }catch(Exception e){
+        } catch (Exception e) {
             System.out.println("ERROR! Invalid month. Set default value.");
             this.month = YearMonth.now();
         }
@@ -23,24 +23,29 @@ public class MonthCalendar {
         this.month = YearMonth.now();
     }
 
-    public void printCalendar(){
-        for (DayOfWeek dayName: DayOfWeek.values())
-            System.out.print(dayName.getDisplayName(TextStyle.SHORT, Locale.ENGLISH)+" ");
+    public void printCalendar() {
+        for (DayOfWeek dayName : DayOfWeek.values()) {
+            System.out.print(dayName.getDisplayName(TextStyle.SHORT, Locale.ENGLISH) + " ");
+        }
         System.out.println();
-        int startPos= LocalDate.of(year,month.getMonthValue(),1).getDayOfWeek().getValue()-1;
-        for(int i = 0; i <startPos;i++)
+        int startPos = LocalDate.of(year, month.getMonthValue(), 1).getDayOfWeek().getValue() - 1;
+        for (int i = 0; i < startPos; i++) {
             System.out.print("    ");
-        for (int i = 1; i<=month.lengthOfMonth();i++){
-            LocalDate monthDay = LocalDate.of(month.getYear(),month.getMonthValue(),i);
-            String dayValue = String.format("%2d  ",monthDay.getDayOfMonth());
-            if(monthDay.equals(LocalDate.now()))
-                System.out.print(CURRENT_DAY_COl+dayValue);
-            else if(monthDay.getDayOfWeek().getValue()==6||monthDay.getDayOfWeek().getValue()==7)
-                System.out.print(DAY_OFF_COl+dayValue);
-            else
-                System.out.print(COMMON_DAY_COl+dayValue);
-            if(monthDay.getDayOfWeek().getValue()==7)
+        }
+        for (int i = 1; i <= month.lengthOfMonth(); i++) {
+            LocalDate monthDay = LocalDate.of(month.getYear(), month.getMonthValue(), i);
+            String dayValue = String.format("%2d  ", monthDay.getDayOfMonth());
+            DayOfWeek monthDayName = monthDay.getDayOfWeek();
+            if (monthDay.equals(LocalDate.now())) {
+                System.out.print(CURRENT_DAY_COl + dayValue);
+            } else if (monthDayName.equals(DayOfWeek.SATURDAY) || monthDayName.equals(DayOfWeek.SUNDAY)) {
+                System.out.print(DAY_OFF_COl + dayValue);
+            } else {
+                System.out.print(COMMON_DAY_COl + dayValue);
+            }
+            if (monthDayName.equals(DayOfWeek.SUNDAY)) {
                 System.out.println();
+            }
         }
     }
 }
